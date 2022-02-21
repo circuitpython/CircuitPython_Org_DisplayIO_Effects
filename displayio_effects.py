12f12d59203a1f0eb42be270984fb6e84dc433b4
Alec Delaney <tekktrik@gmail.com>
2022-02-20 20:40:53 -0500

    Initial commit

---
+++ displayio_effects.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2022 Alec Delaney for CircuitPython Organization
+#
+# SPDX-License-Identifier: MIT
+"""
+`displayio_effects`
+================================================================================
+
+Add some flair to your widgets with effects!
+
+
+* Author(s): Alec Delaney
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+.. todo:: Add links to any specific hardware product page(s), or category page(s).
+  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
+  based on the library's use of either.
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+"""
+
+# imports
+
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/tekktrik/CircuitPython_Org_DisplayIO_Effects.git"
