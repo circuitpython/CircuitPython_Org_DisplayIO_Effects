0b6a6d447311b6762ee104b71a1fe570839db272
Alec Delaney <tekktrik@gmail.com>
2022-02-20 22:11:41 -0500

    Update keywords formatting for setup.py

--- setup.py
+++ setup.py
@@ -54,9 +54,7 @@ setup(
         "Programming Language :: Python :: 3",
     ],
     # What does your project relate to?
-    keywords="adafruit blinka circuitpython micropython displayio_effects effects, displayio, "
-             "widget",
-
+    keywords="adafruit blinka circuitpython micropython displayio_effects effects, displayio, widget",
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     # TODO: IF LIBRARY FILES ARE A PACKAGE FOLDER,
