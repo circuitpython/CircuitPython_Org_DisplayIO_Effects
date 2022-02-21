55525542058e5d9916ea8544e4c760f19773c8ac
Alec Delaney <tekktrik@gmail.com>
2022-02-21 11:26:22 -0500

    Change effect name and variables to "fluctuation"

--- displayio_effects/throttle_effect.py
+++ displayio_effects/throttle_effect.py
@@ -28,66 +28,66 @@ __repo__ = "https://github.com/tekktrik/CircuitPython_Org_DisplayIO_Effects.git"
 
 
 @property
-def throttle_effect(self):
-    """The furtherest the throttle effect can randomly set the widget value relative
+def fluctuation_amplitude(self):
+    """The furtherest the fluctuation effect can randomly set the widget value relative
     to its true position, in either direction.
     """
-    return self._throttle_setting
+    return self._fluctuation_amplitude
 
 
-@throttle_effect.setter
-def throttle_effect(self, setting):
+@fluctuation_amplitude.setter
+def fluctuation_amplitude(self, setting):
     if setting < 0:
-        raise ValueError("Throttle effect setting must be larger than 0")
+        raise ValueError("Fluctuation effect setting must be larger than 0")
     if setting:
-        self._throttle_hold_value = getattr(self, self._value_name)
-    self._throttle_setting = setting
+        self._fluctuation_hold_value = getattr(self, self._value_name)
+    self._fluctuation_amplitude = setting
 
 
 @property
-def throttle_effect_move_rate(self):
-    """The speed at which the throttle effect moves the widget vaalue
+def fluctuation_move_rate(self):
+    """The speed at which the fluctuation effect moves the widget vaalue
     per update"""
 
-    return self._throttle_move_rate
+    return self._fluctuation_move_rate
 
 
-@throttle_effect_move_rate.setter
-def throttle_effect_move_rate(self, rate):
-    self._throttle_move_rate = rate
+@fluctuation_move_rate.setter
+def fluctuation_move_rate(self, rate):
+    self._fluctuation_move_rate = rate
 
 
-def throttle_update(self):
-    """Updates the widget value and propagates the throttle effect refresh"""
+def update_fluctuation(self):
+    """Updates the widget value and propagates the fluctuation effect refresh"""
 
-    if self._throttle_setting == 0:
-        self._throttle_destination = None
+    if self._fluctuation_amplitude == 0:
+        self._fluctuation_destination = None
         return
 
-    if self._throttle_destination in (None, self._throttle_hold_value):
-        limit_bound = self._throttle_setting * 10
-        self._throttle_destination = (
-            random.uniform(-limit_bound, limit_bound) / 10 + self._throttle_hold_value
+    if self._fluctuation_destination in (None, self._fluctuation_hold_value):
+        limit_bound = self._fluctuation_amplitude * 10
+        self._fluctuation_destination = (
+            random.uniform(-limit_bound, limit_bound) / 10 + self._fluctuation_hold_value
         )
 
     value = getattr(self, self._value_name)
     value = (
-        value + self._throttle_move_rate
-        if self._throttle_destination > value
-        else value - self._throttle_move_rate
+        value + self._fluctuation_move_rate
+        if self._fluctuation_destination > value
+        else value - self._fluctuation_move_rate
     )
     setattr(self, self._value_name, value)
 
     threshold_check = (
-        value >= self._throttle_destination
-        if self._throttle_destination >= self._throttle_hold_value
-        else value <= self._throttle_destination
+        value >= self._fluctuation_destination
+        if self._fluctuation_destination >= self._fluctuation_hold_value
+        else value <= self._fluctuation_destination
     )
     if threshold_check:
-        self._throttle_destination = self._throttle_hold_value
+        self._fluctuation_destination = self._fluctuation_hold_value
 
 
-def hook_throttle_effect(widget_class, value_name):
+def hook_fluctuation_effect(widget_class, value_name):
     """Adds the throttle effect for the given classes
 
     :param widget_classes: The widgets that should have this effect hooked
@@ -103,18 +103,18 @@ def hook_throttle_effect(widget_class, value_name):
         from displayio_dial import Dial
         from displayio_effects import throttle_effect
 
-        throttle_effect.hook_throttle_effect(Dial, "value")
+        fluctuation_effect.hook_fluctuation_effect(Dial, "value")
 
     """
 
     setattr(widget_class, "_value_name", value_name)
 
-    setattr(widget_class, "_throttle_destination", None)
-    setattr(widget_class, "_throttle_value", 0)
+    setattr(widget_class, "_fluctuation_destination", None)
+    setattr(widget_class, "_fluctuation_hold_value", 0)
 
-    setattr(widget_class, "throttle_effect", throttle_effect)
-    setattr(widget_class, "_throttle_effect", 0)
-    setattr(widget_class, "throttle_effect_move_rate", throttle_effect_move_rate)
-    setattr(widget_class, "_throttle_move_rate", 0.1)
+    setattr(widget_class, "fluctuation_amplitude", fluctuation_amplitude)
+    setattr(widget_class, "_fluctuation_amplitude", 0)
+    setattr(widget_class, "fluctuation_move_rate", fluctuation_move_rate)
+    setattr(widget_class, "_fluctuation_move_rate", 0.1)
 
-    setattr(widget_class, "throttle_update", throttle_update)
+    setattr(widget_class, "update_fluctuation", update_fluctuation)
